e31b3b97baf1c323997b5e955dc63df66ab8932f
lorenzovon <lazyprogrammerx@gmail.com>
2020-08-31 01:45:41 +0530

    Updates

---
+++ precache-manifest.822079eafd49c854107bd152bb3defce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31650adb012ecc355037f5586e0cc8c7",
+    "url": "/Todo-List-React%20/index.html"
+  },
+  {
+    "revision": "7604966e9ddad1907bad",
+    "url": "/Todo-List-React%20/static/css/main.a46b0c44.chunk.css"
+  },
+  {
+    "revision": "cae303a678086002c6b6",
+    "url": "/Todo-List-React%20/static/js/2.4109f84e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-List-React%20/static/js/2.4109f84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7604966e9ddad1907bad",
+    "url": "/Todo-List-React%20/static/js/main.6e117ccd.chunk.js"
+  },
+  {
+    "revision": "15b1ea26a545739718fd",
+    "url": "/Todo-List-React%20/static/js/runtime-main.95097b18.js"
+  }
+]);
